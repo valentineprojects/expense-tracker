9a10ff9514ebcca76a2b9d68254b7806112a34f8
Valentinas <scenass@gmail.com>
2022-02-17 21:22:28 +0200

    Edited css

---
+++ tracker.js
@@ -0,0 +1,11 @@
+function addExpense(){
+    var name = document.getElementById("name").value;
+    var date = document.getElementById("date").value;
+    var amount = document.getElementById("amount").value;
+
+    var createRow = document.createElement("tr");
+    var createRowData = document.createElement("td");
+    
+
+    
+}
