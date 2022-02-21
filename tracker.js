9dddced5f85474a332a575395d8e0a5bba71fd1c
Valentinas <scenass@gmail.com>
2022-02-21 23:06:13 +0200

    Finally works + delete

--- tracker.js
+++ tracker.js
@@ -1,23 +1,34 @@
 function addExpense(){
-    const table = document.getElementById('table');
-    const row = document.createElement("tr");
-    const data = document.createElement("td");
 
-    const name = data.textContent = document.getElementById("name").value;
-    const date = data.textContent = document.getElementById("date").value;
-    const amount = data.textContent = document.getElementById("amount").value;
-
-    document.getElementById('table').append(data);
-    document.getElementById('name-td').append(name);
-    document.getElementById('date-td').append(date);
-    document.getElementById('amount-td').append(amount);
+    let name = document.getElementById('name').value;
+    let date = document.getElementById('date').value;
+    let amount = document.getElementById('amount').value;
     
+    let table = document.getElementById('table');
+
+    let newRow = table.insertRow(-1);
+    let newNameCell = newRow.insertCell(0);
+    let newDateCell = newRow.insertCell(1);
+    let newAmountCell = newRow.insertCell(2);
+    let newDeleteCell = newRow.insertCell(3);
+
+    let newNameText = document.createTextNode(name);
+    let newDateText = document.createTextNode(date);
+    let newAmountText = document.createTextNode(amount);
+
+    newNameCell.append(newNameText);
+    newDateCell.append(newDateText);
+    newAmountCell.append(newAmountText);
 
     const btn = document.createElement('button');
-    btn.textContent = "Deconste";
-    btn.addEventListener('click', () => row.remove());
-    row.appendChild(btn);
+    btn.textContent = 'Delete';
+    btn.addEventListener('click', function(){
+        newRow.remove()
+        // newNameCell.remove();
+        // newDateCell.remove();
+        // newAmountCell.remove();
+        // newDeleteCell.remove();
+    })
+    newDeleteCell.append(btn);
 
-    document.getElementById('delete-td').appendChild(row);
-    
 }
